cd6f953cae75aa324594d23f9046b784c250b904
Anton Plauche <antonplauche92@gmail.com>
2025-05-06 17:29:52 -0500

    basic wave action working

--- src/components/Water/index.jsx
+++ src/components/Water/index.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import { useStore } from '../../hooks/useStore'
 import { useControls } from 'leva'
 import { useFrame } from '@react-three/fiber'
+import CustomShaderMaterial from "three-custom-shader-material"
+import * as THREE from 'three'
+import vertexShader from "./shaders/vertex.glsl"
+import fragmentShader from "./shaders/fragment.glsl"
 
 const Water = () => {
 
@@ -15,22 +19,28 @@ const Water = () => {
 
   // Interactive water parameters
   const {
-    COLOR_BASE_NEAR, WATER_LEVEL, WAVE_SPEED, WAVE_AMPLITUDE
+    TEXTURE_SIZE, COLOR_BASE_FAR, COLOR_BASE_NEAR, WATER_LEVEL, WAVE_SPEED, WAVE_AMPLITUDE
   } = useControls("Water", {
     COLOR_BASE_NEAR: { value: "#00fccd", label: "Near" },
+    COLOR_BASE_FAR: { value: "#00b1fc", label: "Far" },
     WATER_LEVEL: { value: waterLevel, min: 0.5, max: 5.0, step: 0.1, label: "Water Level" },
     WAVE_SPEED: { value: waveSpeed, min: 0.5, max: 2.0, step: 0.1, label: "Wave Speed" },
     WAVE_AMPLITUDE: { value: waveAmplitude, min: 0.05, max: 0.5, step: 0.05, label: "Wave Amplitude" },
+    TEXTURE_SIZE: { value: 65, min: 1, max: 100, step: 1, label: "Texture Size" },
   })
 
-
+  const COLOR_FAR = useMemo(
+    () => new THREE.Color(COLOR_BASE_FAR),
+    [COLOR_BASE_FAR]
+  )
 
   // Update shader uniforms whenever control values change
   useEffect(() => {
     if (!materialRef.current) return
     materialRef.current.uniforms.uWaveSpeed.value = WAVE_SPEED
     materialRef.current.uniforms.uWaveAmplitude.value = WAVE_AMPLITUDE
-  }, [WAVE_SPEED, WAVE_AMPLITUDE])
+    materialRef.current.uniforms.uColorFar.value = COLOR_FAR
+  }, [WAVE_SPEED, WAVE_AMPLITUDE, COLOR_BASE_FAR])
 
   // Also Update global states on change so other components can use as well
   useEffect(() => {
@@ -57,7 +67,23 @@ const Water = () => {
         position-y={waterLevel} // Moved it down to prevent the visual glitch from plane collision
         receiveShadow
       >
-        <meshStandardMaterial color={"lightblue"} />
+        {/* <meshStandardMaterial color={"lightblue"} /> */}
+        <CustomShaderMaterial
+          ref={materialRef}
+          baseMaterial={THREE.MeshStandardMaterial}
+          vertexShader={vertexShader}
+          fragmentShader={fragmentShader}
+          uniforms={{
+            uTime: { value: 0 },
+            uWaveSpeed: { value: WAVE_SPEED },
+            uWaveAmplitude: { value: WAVE_AMPLITUDE },
+            uColorFar: {value: COLOR_FAR},
+            uTextureSize: {value: TEXTURE_SIZE}
+          }}
+          color={COLOR_BASE_NEAR}
+          transparent
+          opacity={0.4}
+        />
         <planeGeometry args={[256, 256]} />
       </mesh>
     
---
+++ src/components/Water/shaders/vertex.glsl
@@ -0,0 +1,20 @@
+varying vec2 csm_vUv;
+
+uniform float uTime;
+uniform float uWaveSpeed;
+uniform float uWaveAmplitude;
+
+void main() {
+  // Send the uv coordinates to fragmentShader
+  csm_vUv = uv;
+
+  // This moves the whole water up and down slowly, but is a lot more efficient than moving the geometry
+  // Modify the y position based on sine function, oscillating up and down over time
+  float sineOffset = sin(uTime * uWaveSpeed) * uWaveAmplitude;
+
+  // Apply the sine offset to the y component of the position
+  vec3 modifiedPosition = position;
+  modifiedPosition.z += sineOffset; // z used as y because element is rotated
+ 
+  csm_Position = modifiedPosition;
+}
