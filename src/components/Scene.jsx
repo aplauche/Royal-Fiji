e6c6b30dad1082c72f5bd217ddfebd030346701f
Anton Plauche <antonplauche92@gmail.com>
2025-05-07 21:01:40 -0500

    add tree models

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,4 +1,5 @@
 import { Terrain } from "./Terrain/index.jsx"
+import { Palm } from "./Trees/Palm.jsx"
 import Water from "./Water/index.jsx"
 
 
@@ -7,6 +8,9 @@ export const Scene = () => {
     <>
       <Water />
       <Terrain />
+      <Palm position={[2,1,1]}/>
+      <Palm position={[0,1,-1.5]} scale={1.2}/>
+      <Palm position={[-2,1,0.2]}/>
     </>
   )
 }
